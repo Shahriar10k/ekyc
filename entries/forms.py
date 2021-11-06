d35d36bf32fb17bac480a92196f1c7ee48617feb
Shahriar Khan <shahriar.khan018@northsouth.edu>
2021-11-07 02:45:30 +0600

    Add forms.py
    updated forms from branch:ridoy

---
+++ entries/forms.py
@@ -0,0 +1,33 @@
+from django.forms import ModelForm
+from .models import *
+
+
+class Student_info_from(ModelForm):
+    class Meta:
+        model = Student_info
+        fields = '__all__'
+
+class Personal_info_form(ModelForm):
+    class Meta:
+        model = Personal_info
+        fields = '__all__'
+
+class Ssc_equivlent_form(ModelForm):
+    class Meta:
+        model = Ssc_equivlent
+        fields = '__all__'
+
+class Hsc_equivlent_form(ModelForm):
+    class Meta:
+        model = Hsc_equivlent
+        fields = '__all__'
+
+class Grade_history_form(ModelForm):
+    class Meta:
+        model = Grade_history
+        fields = '__all__'
+
+class Financial_history_form(ModelForm):
+    class Meta:
+        model = financial_history
+        fields = '__all__'
