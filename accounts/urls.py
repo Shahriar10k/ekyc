0a297e9e4a04ebba24db76614cc54f1852ae618f
Shahriar Khan <shahriar.khan018@northsouth.edu>
2021-11-06 19:35:12 +0600

    setup login, logout, register page
    + create pages app, index template, edit partials

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.index,name='index')
+    path('login',views.login,name='login'),
+    path('register',views.register,name='register'),
+    path('logout',views.logout,name='logout'),
+    path('dashboard',views.dashboard,name='dashboard'),
 ]
--- ekyc/settings.py
+++ ekyc/settings.py
@@ -130,3 +130,10 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'ekyc/static')]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
+
