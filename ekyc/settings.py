f61030eb811606d601cbbbc6a69530c546cb6bb9
Shahriar Khan <shahriar.khan018@northsouth.edu>
2021-11-06 15:23:12 +0600

    Add accounts app, update settings and urls
    Also update settings.py for postgre DB

--- ekyc/settings.py
+++ ekyc/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'ekyc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ekyc',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST':'localhost',
     }
 }
 
