6f574d0ae5c5fa09204124ab2e42a0c504571ed8
Sabbir Ahmed Sozol <sabbir.sozol@northsouth.edu>
2021-11-18 00:56:23 +0600

    Update template, views of entries app
    Add nav-pills, image, profile redirect

--- entries/views.py
+++ entries/views.py
@@ -10,77 +10,81 @@ from entries.filters import StudentFilter
 
 # Create your views here.
 
-def createEntry(request) : 
-
+def createEntry(request):
     form1 = Student_info_form()
-    
-    if request.method == 'POST':
-        form1 = Student_info_form(request.POST, request.FILES)     
-        if form1.is_valid(): 
-            form1.save()
-            
-            messages.success(request, "Saved")
-            return redirect('dashboard')
-            
 
-    context = {'form1':form1 ,}
+    if request.method == 'POST':
+        form1 = Student_info_form(request.POST, request.FILES)
+        if form1.is_valid():
+            form1.save()
+            f_nsu_id= form1.cleaned_data['nsu_id']
+            stu_obj = Student_info.objects.get(nsu_id=f_nsu_id)
+            stu_uid = stu_obj.id
+            stu_u_obj = Student_info.objects.get(id=stu_uid)
+            print(stu_uid)
+            context = {'stu_uid': stu_u_obj}
+            messages.success(request, "Student Entry Created")
+            return render(request, 'entries/student_entry.html', context)
+
+    context = {'form1': form1, }
 
     return render(request, 'entries/add_entries.html', context)
 
-#filter for student list
+
+# filter for student list
 def StudentList(request):
     student_list = Student_info.objects.all()
-    stuFilter = StudentFilter(request.GET, queryset = student_list)
+    stuFilter = StudentFilter(request.GET, queryset=student_list)
     student_list = stuFilter.qs
 
     context = {'student_list': student_list, 'stuFilter': stuFilter}
     return render(request, 'entries/student_list.html', context)
 
+
+# Student profile General Info
 def studentEntry(request):
     viewdetailsID = request.POST.get('viewdetailsID')
-    #print(viewdetailsID)
-    stu_obj = Student_info.objects.get(nsu_id = viewdetailsID)
+    # print(viewdetailsID)
+    stu_obj = Student_info.objects.get(nsu_id=viewdetailsID)
     stu_uid = stu_obj.id
     stu_u_obj = Student_info.objects.get(id=stu_uid)
     print(stu_uid)
     context = {'stu_uid': stu_u_obj}
     return render(request, 'entries/student_entry.html', context)
 
-#@login_required
-#def StudentProfile(request):
+
+# @login_required
+# def StudentProfile(request):
 #   pass
 #  return render(request, 'student_list/Student_Profile.html')
 
 def updateEntry(request, pk):
-        
-    student = Student_info.objects.get(id = pk)
+    student = Student_info.objects.get(id=pk)
     form1 = Student_info_form(instance=student)
 
     if request.method == 'POST':
-        form1 = Student_info_form(request.POST, request.FILES , instance =student)     
-        if form1.is_valid(): 
+        form1 = Student_info_form(request.POST, request.FILES, instance=student)
+        if form1.is_valid():
             form1.save()
-            
+
             messages.success(request, "Saved")
             return redirect('dashboard')
 
-    context = {'form1':form1 ,}
+    context = {'form1': form1, }
     return render(request, 'entries/update_entries.html', context)
 
-def createCourse(request):
 
+def createCourse(request):
     form1 = Course_form()
-    
+
     if request.method == 'POST':
-        form1 = Course_form(request.POST)     
-        if form1.is_valid(): 
+        form1 = Course_form(request.POST)
+        if form1.is_valid():
             form1.save()
-            
+
             messages.success(request, "Saved")
             return redirect('dashboard')
-            
 
-    context = {'form1':form1 ,}
+    context = {'form1': form1, }
 
     return render(request, 'entries/add_course.html', context)
-
