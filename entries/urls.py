b4f643a218afa14fdd58d8297e5a8bf6d2d5c695
Shahriar Khan <shahriar.khan018@northsouth.edu>
2021-11-18 15:24:27 +0600

    add edit functionality
    update updateEntries function and related templates

--- entries/urls.py
+++ entries/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 
     path('create_entry/',views.createEntry, name='create_entry'),
     path('create_course/',views.createCourse, name='create_course'),
-    path('update_entry<str:pk>/',views.updateEntry, name='update_entry'),
+    path('update_entry/',views.updateEntry, name='update_entry'),
     path('',views.StudentList,name='StudentList'),
     path('student_entry/',views.studentEntry, name='student_entry'),
     #path('addentry',views.addentry, name='addentry'),
--- entries/views.py
+++ entries/views.py
@@ -17,11 +17,11 @@ def createEntry(request):
         form1 = Student_info_form(request.POST, request.FILES)
         if form1.is_valid():
             form1.save()
+            #use the input nsu id to fetch student
             f_nsu_id= form1.cleaned_data['nsu_id']
             stu_obj = Student_info.objects.get(nsu_id=f_nsu_id)
             stu_uid = stu_obj.id
             stu_u_obj = Student_info.objects.get(id=stu_uid)
-            print(stu_uid)
             context = {'stu_uid': stu_u_obj}
             messages.success(request, "Student Entry Created")
             return render(request, 'entries/student_entry.html', context)
@@ -29,6 +29,42 @@ def createEntry(request):
     context = {'form1': form1, }
 
     return render(request, 'entries/add_entries.html', context)
+
+#update entry
+def updateEntry(request): 
+
+    #if POST coming from student list "edit" button then fetch intance using uuid
+    if 'viewdetailsID' in request.POST:
+        uid = request.POST.get('viewdetailsID')
+        student = Student_info.objects.get(id=uid)
+        form1 = Student_info_form(instance=student)
+    #if POST coming from editing the form then fetch instance using nsu id 
+    else:
+        nsu_id= request.POST.get('nsu_id')
+        student=Student_info.objects.get(nsu_id=nsu_id)
+        form1 = Student_info_form(instance=student)
+    context = {'form1': form1, }
+    
+
+    if request.method == 'POST' :
+        form1 = Student_info_form(request.POST, request.FILES, instance=student)
+        print("here")
+        print(form1)
+        if form1.is_valid():
+            form1.save()
+
+            #use the input nsu id to fetch student
+            f_nsu_id= form1.cleaned_data['nsu_id']
+            stu_obj = Student_info.objects.get(nsu_id=f_nsu_id)
+            stu_uid = stu_obj.id
+            stu_u_obj = Student_info.objects.get(id=stu_uid)
+            context = {'stu_uid': stu_u_obj}
+            messages.success(request, "Student Entry Updated")
+            return render(request, 'entries/student_entry.html', context)
+
+    
+    
+    return render(request, 'entries/update_entries.html', context)
 
 
 # filter for student list
@@ -44,11 +80,11 @@ def StudentList(request):
 # Student profile General Info
 def studentEntry(request):
     viewdetailsID = request.POST.get('viewdetailsID')
-    # print(viewdetailsID)
+    #print(viewdetailsID)
     stu_obj = Student_info.objects.get(nsu_id=viewdetailsID)
     stu_uid = stu_obj.id
     stu_u_obj = Student_info.objects.get(id=stu_uid)
-    print(stu_uid)
+    #print(stu_uid)
     context = {'stu_uid': stu_u_obj}
     return render(request, 'entries/student_entry.html', context)
 
@@ -57,21 +93,6 @@ def studentEntry(request):
 # def StudentProfile(request):
 #   pass
 #  return render(request, 'student_list/Student_Profile.html')
-
-def updateEntry(request, pk):
-    student = Student_info.objects.get(id=pk)
-    form1 = Student_info_form(instance=student)
-
-    if request.method == 'POST':
-        form1 = Student_info_form(request.POST, request.FILES, instance=student)
-        if form1.is_valid():
-            form1.save()
-
-            messages.success(request, "Saved")
-            return redirect('dashboard')
-
-    context = {'form1': form1, }
-    return render(request, 'entries/update_entries.html', context)
 
 
 def createCourse(request):
