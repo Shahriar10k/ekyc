481692d68f0b23c9f4725074c300d8adab21018a
Sabbir Ahmed Sozol <sabbir.sozol@northsouth.edu>
2021-11-19 10:45:20 +0600

    Fix Template Grade Info

--- entries/views.py
+++ entries/views.py
@@ -127,7 +127,9 @@ def studentEntry(request):
     else:
         stu_uid = mydata['stu_uid']
     
-    # fetching Student_info object of the student using UUID
+    stu_obj = Student_info.objects.get(nsu_id=stu_id)
+    stu_uid = stu_obj.id
+    # fetching uid object of student info
     stu_u_obj = Student_info.objects.get(id=stu_uid)
     context = {'stu_uid': stu_u_obj}
 
