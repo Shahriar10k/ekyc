794b53158e8684eb470c41f3458b80cc41736798
Shahriar Khan <shahriar.khan018@northsouth.edu>
2021-11-28 10:49:33 +0600

    add course update functionality and course list
    add template and filters for course list and update

--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('course_list',views.CourseList,name='course_list'),
+    path('update_course',views.updateCourse,name='update_course'),
 ]
